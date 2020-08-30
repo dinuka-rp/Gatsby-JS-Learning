a62939cba404075f29394fad092e6d25e1167bf4
dinuka-rp <drpiyadigama@gmail.com>
2020-08-31 05:03:59 +0530

    Used CSS Modules for styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,54 @@
 import React from "react"
 import { Link } from "gatsby"
 
+// import "./header.module.scss"
+import HeaderStyles from "./header.module.scss" // css module
+
 const Header = () => {
   return (
-    <header>
-      <p>Personal Profile</p>
+    <header className={HeaderStyles.header}>
+      <h1>
+        <Link to="/" className={HeaderStyles.title}>
+          Personal Profile
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={HeaderStyles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              to="/"
+              className={HeaderStyles.navItem}
+              activeClassName={HeaderStyles.activeNavItem}
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link
+              to="/blog"
+              className={HeaderStyles.navItem}
+              activeClassName={HeaderStyles.activeNavItem}
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              to="/about"
+              className={HeaderStyles.navItem}
+              activeClassName={HeaderStyles.activeNavItem}
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              to="/contact"
+              className={HeaderStyles.navItem}
+              activeClassName={HeaderStyles.activeNavItem}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
