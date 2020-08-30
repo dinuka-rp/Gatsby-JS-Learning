c2f975d3bb5fb89422abfb5fdb47b71130b9bf73
dinuka-rp <drpiyadigama@gmail.com>
2020-08-31 04:18:34 +0530

    Implemented a layout to use in multiple pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
-import Footer from "../components/footer"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 const AboutPage = () => {
   return (
-    <div>
-      <Header />
-
+    <Layout>
       <h1>About Me</h1>
       <p>I'm a Full Stack Developer.</p>
 
       <p>
         Need a developer? <Link to="/contact">Contact me.</Link>
       </p>
-
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-import Footer from "../components/footer"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <div>
-      <Header />
-      
+    <Layout>
       <h1>Hello there!</h1>
       <p>I'm Dinuka, living in beautiful Sri Lanka</p>
       <p>
         Need a developer? <Link to="/contact">Contact me.</Link>
       </p>
-
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 // index is taken as the home page automatically
