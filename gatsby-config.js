5248f73966a4cb126d2fa481afd31de792ea972b
dinuka-rp <drpiyadigama@gmail.com>
2020-11-01 01:15:15 +0530

    Implement displaying images from marrkdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,14 +11,29 @@ module.exports = {
     author: "Dinuka Piyadigama",
   },
   plugins: [
-    'gatsby-plugin-sass',
+    "gatsby-plugin-sass",
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: 'src',
-        path:  `${__dirname}/src/`
+        name: "src",
+        path: `${__dirname}/src/`,
       },
     },
-    'gatsby-transformer-remark'
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,  // maxWidth of an image
+              linkImagesToOriginal:false    // to prevent clicking images to redirect to the actual location of the image
+            },
+          },
+        ],
+      },
+    },
   ],
 }
--- src/posts/gatsby.md
+++ src/posts/gatsby.md
@@ -5,6 +5,8 @@ date: "2020-09-16"
 
 I just launched a new bootcamp!
 
+![Winter](./winter.jpg)
+
 ## Topics covered
 1. Gatsby
 2. Graphql
